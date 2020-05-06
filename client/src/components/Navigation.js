2073daaee3e0d2185821b2222b66582844e75570
Brianna Workman <brianna.workman@hotmail.com>
2020-05-05 22:30:38 -0400

    adds nav icons and styles navigation bar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import '../App.css';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import profile from '../assets/profile.png';
+import mic from '../assets/microphone.png';
+import music from '../assets/music.png';
 
 function Navigation() {
   return (
-    <>
-      <Link to='/'>My Profile</Link>
-      <Link to='/artists'>Artists</Link>
-      <Link to='/tracks'>Tracks</Link>
-    </>
+    <div className='navigation'>
+      <div className='logo'>
+        <Link to='/'>
+          <div>
+            <img src={logo} />
+          </div>
+        </Link>
+      </div>
+      <div className='links'>
+        <Link to='/' className='nav-link'>
+          <div>
+            <img src={profile} />
+            <p>Profile</p>
+          </div>
+        </Link>
+        <Link to='/artists' className='nav-link'>
+          <div>
+            <img src={mic} />
+            <p>Artists</p>
+          </div>
+        </Link>
+        <Link to='/tracks' className='nav-link'>
+          <div>
+            <img src={music} />
+            <p>Tracks</p>
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 }
 
